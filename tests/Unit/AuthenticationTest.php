953f647d74642091557c117e2c979814bd5479af
jamirraert0122 <c.baquiano22@gmail.com>
2025-10-17 10:41:16 +0800

    initial commit

---
+++ tests/Unit/AuthenticationTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class AuthenticationTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function user_can_login(): void
+    {
+        $this->assertTrue(true);
+    }
+}
